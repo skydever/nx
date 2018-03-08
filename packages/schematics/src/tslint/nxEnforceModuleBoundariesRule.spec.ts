36839f6b119fdbb43ff971c5d80fe98c53ff15f7
Thomas Skalnik <thomas.skalnik@gmail.com>
2018-03-09 00:25:29 +0100

    test(schematics): fix test data

--- packages/schematics/src/tslint/nxEnforceModuleBoundariesRule.spec.ts
+++ packages/schematics/src/tslint/nxEnforceModuleBoundariesRule.spec.ts
@@ -6,10 +6,10 @@ import { Rule } from './nxEnforceModuleBoundariesRule';
 describe('Enforce Module Boundaries', () => {
   it('should not error when everything is in order', () => {
     const failures = runRule(
-      { allow: ['@mycompany/mylib/deep'], lazyLoad: ['myliblazy'] },
+      { allow: ['@mycompany/libgroup/mylib/deep'], lazyLoad: ['myliblazy'] },
       `
-      import '@mycompany/mylib';
-      import '@mycompany/mylib/deep';
+      import '@mycompany/libgroup/mylib';
+      import '@mycompany/libgroup/mylib/deep';
       import '../blah';
     `
     );
@@ -19,37 +19,37 @@ describe('Enforce Module Boundaries', () => {
 
   describe('relative imports', () => {
     it('should not error when relatively importing the same library', () => {
-      const failures = runRuleToCheckForRelativeImport('import "../mylib2"');
+      const failures = runRuleToCheckForRelativeImport('import "../libgroup/mylib2"');
       expect(failures.length).toEqual(0);
     });
 
     it('should not error when relatively importing the same library (index file)', () => {
-      const failures = runRuleToCheckForRelativeImport('import "../../mylib"');
+      const failures = runRuleToCheckForRelativeImport('import "../../libgroup/mylib"');
       expect(failures.length).toEqual(0);
     });
 
     it('should error when relatively importing another library', () => {
-      const failures = runRuleToCheckForRelativeImport('import "../../../libs/mylib2"');
+      const failures = runRuleToCheckForRelativeImport('import "../../../../libs/libgroup/mylib2"');
       expect(failures.length).toEqual(1);
       expect(failures[0].getFailure()).toEqual('library imports must start with @mycompany/');
     });
 
     it('should error on a relatively importing another library (in the same dir)', () => {
-      const failures = runRuleToCheckForRelativeImport('import "../../mylib2"');
+      const failures = runRuleToCheckForRelativeImport('import "../../../libgroup/mylib2"');
       expect(failures.length).toEqual(1);
       expect(failures[0].getFailure()).toEqual('library imports must start with @mycompany/');
     });
   });
 
   it('should error on absolute imports into libraries without using the npm scope', () => {
-    const failures = runRule({}, `import 'libs/mylib';`);
+    const failures = runRule({}, `import 'libs/libgroup/mylib';`);
 
     expect(failures.length).toEqual(1);
     expect(failures[0].getFailure()).toEqual('library imports must start with @mycompany/');
   });
 
   it('should error about deep imports into libraries', () => {
-    const failures = runRule({}, `import '@mycompany/mylib/blah';`);
+    const failures = runRule({}, `import '@mycompany/libgroup/mylib/blah';`);
 
     expect(failures.length).toEqual(1);
     expect(failures[0].getFailure()).toEqual('deep imports into libraries are forbidden');
@@ -62,39 +62,48 @@ describe('Enforce Module Boundaries', () => {
        import '@mycompany/reporting-other';
        import '@mycompany/reporting';
        `,
-      ['reporting', 'reporting-dashboard-ui']
+      ['reporting', 'reporting-dashboard-ui'],
+      [],
+      ['libs/reporting', 'libs/reporting-other', 'libs/reporting-dashboard-ui']
     );
 
     expect(failures.length).toEqual(0);
 
-    // Make sure it works regardless of order of app names list
+    // Make sure it works regardless of order of lib/root names list
     failures = runRule(
       {},
       `import '@mycompany/reporting-dashboard-ui';
        import '@mycompany/reporting-other';
        import '@mycompany/reporting';`,
-      ['reporting-dashboard-ui', 'reporting']
+      ['reporting-dashboard-ui', 'reporting'],
+      [],
+      ['libs/reporting-dashboard-ui', 'libs/reporting-other', 'libs/reporting']
     );
 
     expect(failures.length).toEqual(0);
   });
 
   it('should error on importing a lazy-loaded library', () => {
-    const failures = runRule({ lazyLoad: ['mylib'] }, `import '@mycompany/mylib';`);
+    const failures = runRule({ lazyLoad: ['libgroup/mylib'] }, `import '@mycompany/libgroup/mylib';`);
 
     expect(failures.length).toEqual(1);
     expect(failures[0].getFailure()).toEqual('imports of lazy-loaded libraries are forbidden');
   });
 
   it('should error on importing an app', () => {
-    const failures = runRule({ lazyLoad: ['mylib'] }, `import '@mycompany/myapp';`, [], ['myapp']);
+    const failures = runRule({ lazyLoad: ['libgroup/mylib'] }, `import '@mycompany/myapp';`);
 
     expect(failures.length).toEqual(1);
     expect(failures[0].getFailure()).toEqual('imports of apps are forbidden');
   });
 
-  it('should error on importing a lib that has the same prefix as an app', () => {
-    const noFailures = runRule({ lazyLoad: [] }, `import '@mycompany/myapp/mylib';`, ['myapp/mylib'], ['myapp']);
+  it('should not error on importing a lib that has the same prefix as an app', () => {
+    const noFailures = runRule({ 
+      lazyLoad: [] }, 
+      `import '@mycompany/myapp/mylib';`, 
+      ['myapp/mylib'], 
+      ['myapp'],
+      ['libs/myapp/mylib', 'apps/myapp']);
 
     expect(noFailures.length).toEqual(0);
   });
@@ -103,9 +112,9 @@ describe('Enforce Module Boundaries', () => {
 function runRule(
   ruleArguments: any,
   content: string,
-  libNames: string[] = ['libgroupdir/mylib', 'libgroupdir/mylib2', 'libgroupdir/myliblazy'],
+  libNames: string[] = ['libgroup/mylib', 'libgroup/mylib2', 'libgroup/myliblazy'],
   appNames: string[] = ['myapp', 'myapp2'],
-  roots: string[] = ['libs/mylib', 'libs/mylib2', 'apps/myapp', 'apps/myapp2']
+  roots: string[] = ['libs/libgroup/mylib', 'libs/libgroup/mylib2', 'apps/myapp', 'apps/myapp2']
 ): RuleFailure[] {
   const options: any = {
     ruleArguments: [ruleArguments],
@@ -114,12 +123,12 @@ function runRule(
   };
 
   const sourceFile = ts.createSourceFile(
-    '/proj/apps/myapp/src/main.ts',
+    '/proj/mycompany/apps/myapp/src/main.ts',
     content,
     ts.ScriptTarget.Latest,
     true
   );
-  const rule = new Rule(options, '/proj', 'mycompany', libNames, appNames, roots);
+  const rule = new Rule(options, '/proj/mycompany', 'mycompany', libNames, appNames, roots);
   return rule.apply(sourceFile);
 }
 
@@ -131,18 +140,18 @@ function runRuleToCheckForRelativeImport(content: string): RuleFailure[] {
   };
 
   const sourceFile = ts.createSourceFile(
-    '/proj/libs/mylib/src/module.t',
+    '/proj/mycompany/libs/libgroup/mylib/src/module.t',
     content,
     ts.ScriptTarget.Latest,
     true
   );
   const rule = new Rule(
     options,
-    '/proj',
+    '/proj/mycompany',
     'mycompany',
-    ['mylib', 'mylib2', 'myliblazy'],
+    ['libgroup/mylib', 'libgroup/mylib2', 'libgroup/myliblazy'],
     ['myapp', 'myapp2'],
-    ['libs/mylib', 'libs/mylib2', 'apps/myapp', 'apps/myapp2']
+    ['libs/libgroup/mylib', 'libs/libgroup/mylib2', 'apps/myapp', 'apps/myapp2']
   );
   return rule.apply(sourceFile);
 }
