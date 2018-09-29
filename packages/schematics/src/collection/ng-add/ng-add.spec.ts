55f0ccccfaf8426b03106d5137c2f54da2d3f4d0
skydever <thomas.skalnik@gmail.com>
2018-09-29 23:23:41 +0200

    fix: write using fs-extra

--- packages/schematics/src/collection/ng-add/ng-add.spec.ts
+++ packages/schematics/src/collection/ng-add/ng-add.spec.ts
@@ -2,7 +2,7 @@ import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 import { Tree, VirtualTree } from '@angular-devkit/schematics';
 import { readJsonInTree } from '@nrwl/schematics/src/utils/ast-utils';
-import { writeFileSync } from 'fs';
+import { writeJsonSync } from 'fs-extra';
 
 describe('workspace', () => {
   const schematicRunner = new SchematicTestRunner(
@@ -80,7 +80,7 @@ describe('workspace', () => {
   it('should convert all fileReplacement paths in angular.json', () => {
     appTree.create('/package.json', JSON.stringify({}));
     appTree.create('/src/tslint.json', JSON.stringify({}));
-    writeFileSync('/src/tslint.json', '{}');
+    writeJsonSync('./src/tslint.json', '{}');
     appTree.create('/e2e/protractor.conf.js', '');
     appTree.create(
       '/angular.json',
